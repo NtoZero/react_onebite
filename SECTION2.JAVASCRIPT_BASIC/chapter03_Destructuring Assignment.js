25b464e90f14717cf85b0f45df315fbc0b394291
st.lee <dev.st1027@gmail.com>
2024-08-28 22:42:35 +0900

    feat: S2.Spread 연산자와 Rest 매개변수까지

---
+++ SECTION2.JAVASCRIPT_BASIC/chapter03_Destructuring Assignment.js
@@ -0,0 +1,31 @@
+/* 구조 분해 할당(Destructuring Assignment) */
+
+
+
+// 1. 배열의 구조 분해 할당
+let arr = [1, 2, 3];
+
+let [one, two, three, four = 4] = arr;
+
+// 2. 객체의 구조 분해 할당
+let person = {
+  name: "이정환",
+  age: 27,
+  hobby: "테니스",
+};
+
+let {
+  age: myAge,
+  hobby,
+  name,
+  extra = "hello",
+} = person;
+// console.log(myAge, hobby, name, extra);
+
+// 3. 객체 구조 분해 할당을 이용해서 함수의 매개변수를 받는 방법
+const func = ({ name, age, hobby, extra }) => {
+    // 이정환 27 테니스 undefined
+  console.log(name, age, hobby, extra);
+};
+
+func(person);
