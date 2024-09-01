571bbc398aae197f2c47e69fd31e189789582856
st.lee <dev.st1027@gmail.com>
2024-09-01 20:35:23 +0900

    feat: node.js로 자바스크립트 연습

--- S3.Node.js/src/index.js
+++ S3.Node.js/src/index.js
@@ -1,30 +1,22 @@
-/* CJS */
-/* const moduleData = require("./math");
+// 1. 배열의 구조분해 할당
+let arr = [1, 2, 3, 4, 5];
 
-console.log(moduleData.add(1, 2));
-console.log(moduleData.sub(1, 2));
+let [one, two, three, four = 4] = arr;
+console.log(one, two, three, four);
 
-// 구조 분해 할당 이용
-const {add, sub} = require("./maht");
+// 2. 객체의 구조분해 할당
 
-console.log(add(2,3));
-console.log(sub(2,3)); */
+let person = {
+  name: "st.lee",
+  age: 20,
+  hobby: "coding",
+};
 
-/* ESM */
-// import { add, sub } from "./math.js";
+let { name, age, hobby, hobby2 = "helth" } = person;
+console.log(name, age, hobby, hobby2);
 
-// console.log(add(1, 2));
-// console.log(sub(4, 2));
+function funcB(one, two, ...ds) {
+  console.log(ds);
+}
 
-// default function은 이름을 마음대로 정의 가능
-// import mul from "./math.js";
-
-// import mul, { add, sub } from "./math.js";
-// console.log(mul(2, 5));
-
-/* 라이브러리 이용하기 */
-
-import randomColor from "randomcolor";
-
-const color = randomColor();
-console.log(color);
+funcB(...arr);
