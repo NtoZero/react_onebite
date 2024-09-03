b7dd9a9e8d9e8a66ca4cebf3267fe2b765e9f8cc
st.lee <dev.st1027@gmail.com>
2024-09-03 22:00:12 +0900

    refactor(S5.React 입문): 7-1. State를 Props로 전달하기- State 컴포넌트 분리

--- S5.React_Intro/src/App.jsx
+++ S5.React_Intro/src/App.jsx
@@ -1,32 +1,15 @@
 import './App.css'
+import Bulb from "./components/Bulb.jsx";
+import Counter from "./components/Counter.jsx";
 import {useState} from "react";
-
-/* 함수 컴포넌트 */
 
 /*부모 컴포넌트 App */
 function App() {
 
-    const [state, setState] = useState(0);
-    const [light, setLight] = useState("OFF");
-    // let light = "OFF"; // 단순 변수 값을 변경한다고 리렌더링을 발생시키지 않음.
-
-  return (
+    return (
     <>
-        <div>
-            <h1>{light}</h1>
-            <button onClick={() => {
-                setLight(light === "OFF" ? "ON" : "OFF");
-                // light = light === "ON" ? "OFF" : "ON";
-                // console.log(light);
-            }}>전구 {light === "ON" ? "끄기" : "켜기"}
-            </button>
-        </div>
-        <div>
-            <h1>{state}</h1>
-            <button onClick = {() =>{
-                setState(state + 1);
-            }}> + 버튼 </button>
-        </div>
+        <Bulb />
+        <Counter />
     </>
   )
 }
