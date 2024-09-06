94b9e9198b68e2f631ec073f0f6c15d4986a7d2f
st.lee <dev.st1027@gmail.com>
2024-09-06 21:16:55 +0900

    config(S5.React 입문): State로 사용자 입력 관리하기

--- S5.React_Intro/src/App.jsx
+++ S5.React_Intro/src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import Bulb from "./components/Bulb.jsx";
-import Counter from "./components/Counter.jsx";
-import { useState } from "react";
+import Register from "./components/Register.jsx";
 
 /*부모 컴포넌트 App */
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
