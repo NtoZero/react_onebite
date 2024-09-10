4b12a46d74cb2d5c202fe73a1d43e60bd96b507c
st.lee <dev.st1027@gmail.com>
2024-09-10 22:49:17 +0900

    feat(S7. 라이프사이클): 3 useEffect로 라이프사이클 제어하기

--- S7.LifeCycle/src/App.jsx
+++ S7.LifeCycle/src/App.jsx
@@ -1,15 +1,29 @@
 import Viewer from "./components/Viewer.jsx";
 import Controller from "./components/Controller.jsx";
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Even from "./components/Even.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
+  //1. Mount : 최초 렌더링
   useEffect(() => {
-    console.log(`count: ${count} / input : ${input}`);
-  }, [count, input]);
+    console.log("mount");
+  }, []);
+
+  //2. Update : props, state 변화
+  // 최초 Mount될 때 Update 생명주기의 useEffect를 무시하기 위함.
+  const isMount = useRef(false);
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  }); // 의존성 배열이 없으면 항상 업데이트
+  //3. UnMount : 소멸
 
   const onClickButton = (value) => {
     setCount(count + value); //  비동기 (호출만)
@@ -29,6 +43,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
