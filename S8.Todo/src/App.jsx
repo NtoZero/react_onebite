0ddb89f36e4f50482b78640026c06828ee91c8ad
st.lee <dev.st1027@gmail.com>
2024-09-11 22:17:36 +0900

    feat(S8. Todo 프로젝트): 3,4 Create Todo 추가하기

--- S8.Todo/src/App.jsx
+++ S8.Todo/src/App.jsx
@@ -1,16 +1,49 @@
+import { useState, useRef } from "react";
 import "./App.css";
 import Editor from "./components/Editor.jsx";
 import Header from "./components/Header.jsx";
 import List from "./components/List.jsx";
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().toDateString(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
 }
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+];
 
 export default App;
