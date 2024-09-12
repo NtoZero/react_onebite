a6749f56efe4deeed150cc0843630f6f77a4d3ba
st.lee <dev.st1027@gmail.com>
2024-09-12 20:22:30 +0900

    feat(S8. Todo 프로젝트): 6. Update - 투두 수정하기 : 강사님 버전 (onUpdate 활용) => 코드 훨씬 간결 but 전체 렌더링되는 단점

--- S8.Todo/src/components/TodoItem.jsx
+++ S8.Todo/src/components/TodoItem.jsx
@@ -1,18 +1,13 @@
 import "./Todo.css";
-import { useState } from "react";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
-  const [done, setDone] = useState(isDone);
-  const changeDone = (isDone) => {
-    setDone(!done);
-  };
-  const onChangeIsDone = (e) => {
-    return changeDone(e.target.value);
+  const onChangeCheckbox = () => {
+    onUpdate(id);
   };
 
   return (
     <div className="TodoItem">
-      <input onChange={onChangeIsDone} checked={done} type="checkbox" />
+      <input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
