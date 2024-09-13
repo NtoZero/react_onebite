a711c75e36cdf35a71bbfba08d193a37f9a418db
st.lee <dev.st1027@gmail.com>
2024-09-13 18:58:20 +0900

    feat(S10. Optimization): 2. useMemo와 연산 최적화 (활용 전)

--- S10.Optimization/src/components/List.jsx
+++ S10.Optimization/src/components/List.jsx
@@ -17,9 +17,31 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
+  const getAnalyzedData = () => {
+    console.log("getAnalzedData 호출!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  };
+
+  // 해당 함수는 컴포넌트 내에서 바로 호출되고 있기 때문에 List 컴포넌트가 리렌더링 될 때마다 계속 호출될 것이다.
+  // 검색바에 검색만해도 의미없는 함수 호출이 여러 번 발생하게 되는 것이다. (글자당 한 번씩 발생)
+  const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
   return (
     <div className="List">
       <h4>Todo List ☘️☘️</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>doneCount: {doneCount}</div>
+        <div>notDoneCount: {notDoneCount}</div>
+      </div>
       <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
