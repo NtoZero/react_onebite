dae1dd6d2860e869ecc116dd732bb47f1a7e11dd
st.lee <dev.st1027@gmail.com>
2024-09-13 19:10:15 +0900

    feat(S10. Optimization): 2. useMemo와 연산 최적화 (활용 후)

--- S10.Optimization/src/components/List.jsx
+++ S10.Optimization/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem.jsx";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -17,7 +17,7 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   const filteredTodos = getFilteredData();
 
-  const getAnalyzedData = () => {
+  /*const getAnalyzedData = () => {
     console.log("getAnalzedData 호출!");
     const totalCount = todos.length;
     const doneCount = todos.filter((todo) => todo.isDone).length;
@@ -32,7 +32,20 @@ const List = ({ todos, onUpdate, onDelete }) => {
 
   // 해당 함수는 컴포넌트 내에서 바로 호출되고 있기 때문에 List 컴포넌트가 리렌더링 될 때마다 계속 호출될 것이다.
   // 검색바에 검색만해도 의미없는 함수 호출이 여러 번 발생하게 되는 것이다. (글자당 한 번씩 발생)
-  const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+  const { totalCount, doneCount, notDoneCount } = getAnalyzedData();*/
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalzedData 호출!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
 
   return (
     <div className="List">
