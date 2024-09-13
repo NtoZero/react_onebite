9e00bd7eec5587f1f77cce189246d01997d7d0ec
st.lee <dev.st1027@gmail.com>
2024-09-13 20:18:25 +0900

    feat(S10. Optimization): 3. React.memo (3) - memo의 두 번째 인자 활용하여 사용자 정의 비교 수행하기

--- S10.Optimization/src/components/TodoItem.jsx
+++ S10.Optimization/src/components/TodoItem.jsx
@@ -20,4 +20,17 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default memo(TodoItem);
+// 고차 컴포넌트 (HOC : Higher Order Component)
+//React에서 컴포넌트를 위한 디자인 패턴으로, 컴포넌트를 인수로 받아 새로운 컴포넌트를 반환하는 함수
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+  // T -> Props 바뀌지 않음 -> 리렌더링 X
+  // F -> Props 바뀜 -> 리렌더링 O
+
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+
+  return true;
+});
