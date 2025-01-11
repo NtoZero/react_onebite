00ef2ce3bdbe821d3543070be2d4b55e9b072a52
st.lee <dev.st1027@gmail.com>
2025-01-11 22:35:31 +0900

    feat(S7. LifeCycle): re : 복습 - react 라이프 사이클 이해, useEffect와 useRef 활용

--- S7.LifeCycle/src/App.jsx
+++ S7.LifeCycle/src/App.jsx
@@ -4,25 +4,28 @@ import "./App.css";
 import { useState, useEffect, useRef } from "react";
 import Even from "./components/Even.jsx";
 
-function App2() {
+function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
   //1. Mount : 최초 렌더링
   useEffect(() => {
-    console.log("mount");
+    console.log("App component Mounted!");
   }, []);
 
   //2. Update : props, state 변화
   // 최초 Mount될 때 Update 생명주기의 useEffect를 무시하기 위함.
-  const isMount = useRef(false);
+  const isMount = useRef(false); //반드시 ref 참조를 dom 요소에 넣을 필요는 없구나! 단순 값 저장을 위해서 사용되기도 한다.
   useEffect(() => {
     if (!isMount.current) {
+      // useRef 객체는 컴포넌트가 리렌더링되어도 동일한 참조를 유지함.
+      // current 속성은 리액트가 해당 ref에 대한 값을 보관하는 속성임.
+      // 최초 렌더링되었으면 해당 컴포넌트가 이미 마운트된 상태라는 것을 명시하기 위해 isMount의 값을 true로 변경
       isMount.current = true;
       return;
     }
-    console.log("update");
-  }); // 의존성 배열이 없으면 항상 업데이트
+    console.log("App component updated!");
+  }); // 의존성 배열이 없으면 **상태나 props가 변경될 때마다** 항상 콜백이 실행됨 (권장x)
   //3. UnMount : 소멸
 
   const onClickButton = (value) => {
--- S7.LifeCycle/src/components/Even.jsx
+++ S7.LifeCycle/src/components/Even.jsx
@@ -2,11 +2,13 @@ import { useEffect } from "react";
 
 const Even = () => {
   useEffect(() => {
-    // 클린업, 정리함수
+    console.log("Even Component Mounted!");
+
+    // return에 포함된 것은 클린업, 정리함수
     return () => {
-      console.log("unmount");
+      console.log("Even Component unmounted!");
     };
-  }, []);
+  }, []); // 의존성 배열이 비어있으므로 렌더링 시에 최초 실행
   return <div>짝수입니다.</div>;
 };
 
